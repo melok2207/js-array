883d38b96abad9279975c3027ae4c85e175314c0
melok2207 <132545373+melok2207@users.noreply.github.com>
2024-10-08 09:42:17 +0300

    Update task3.js

--- tasks/task3.js
+++ tasks/task3.js
@@ -1,6 +1,9 @@
 // З масива людей(people) Вам потрібно знайти людину за іменем(name)
 function findByName(people, name) {
-  // Ваш код
+  
+  return people.find(function(person) {
+    return person.name === name;
+  });
 }
 
 module.exports = findByName;
